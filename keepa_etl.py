9f7c903e9b04f65cf77c2a3faba709428d4333fd
AlexBomber12 <48256657+AlexBomber12@users.noreply.github.com>
2025-07-01 01:59:16 +0200

    Refine Keepa ETL script with optional live mode

--- keepa_etl.py
+++ keepa_etl.py
@@ -9,26 +9,29 @@ minio_access=os.environ['MINIO_ACCESS_KEY']
 minio_secret=os.environ['MINIO_SECRET_KEY']
 pg_dsn=os.environ['PG_DSN']
 
-api=keepa.Keepa(key)
 start=time.time()
-params={
-    "current_SALES_lte":80000,
-    "current_BUY_BOX_SHIPPING_gte":2000,
-    "current_COUNT_NEW_lte":10
-}
-asins=api.product_finder(params,domain="IT",n_products=20000)
+if os.getenv('ENABLE_LIVE')=='1':
+    api=keepa.Keepa(key)
+    params={
+        "current_SALES_lte":80000,
+        "current_BUY_BOX_SHIPPING_gte":2000,
+        "current_COUNT_NEW_lte":10
+    }
+    data=json.dumps(api.product_finder(params,domain='IT',n_products=20000)).encode()
+else:
+    with open('fixtures/keepa_sample.json') as f:
+        data=json.dumps(json.load(f)).encode()
 duration=time.time()-start
 date=datetime.date.today()
 path=f"raw/{date:%Y/%m/%d}/asins.json"
-bucket="keepa"
-data=json.dumps(asins).encode()
+bucket='keepa'
 mc=Minio(minio_endpoint,access_key=minio_access,secret_key=minio_secret,secure=False)
 if not mc.bucket_exists(bucket):
     mc.make_bucket(bucket)
 mc.put_object(bucket,path,io.BytesIO(data),len(data))
 conn=psycopg2.connect(pg_dsn)
 cur=conn.cursor()
-cur.execute("INSERT INTO etl_log(date,asin_count,duration_sec) VALUES (%s,%s,%s)",(date,len(asins),duration))
+cur.execute('INSERT INTO etl_log(date,asin_count,duration_sec) VALUES (%s,%s,%s)',(date,len(json.loads(data)),duration))
 conn.commit()
 cur.close()
 conn.close()
