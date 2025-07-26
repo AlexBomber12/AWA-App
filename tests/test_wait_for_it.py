9dac4e454827eb3348135339ea4b1fe60fcdec0d
Alex_Bomber <48256657+AlexBomber12@users.noreply.github.com>
2025-07-26 22:28:45 +0200

    Fix wait-for-it invocation

--- tests/test_wait_for_it.py
+++ tests/test_wait_for_it.py
@@ -5,6 +5,16 @@ import subprocess
 def test_wait_for_it_exec() -> None:
     script = pathlib.Path("services/etl/wait-for-it.sh")
     proc = subprocess.run(
-        ["bash", str(script), "localhost:0", "echo", "ok"], capture_output=True
+        [
+            "bash",
+            str(script),
+            "localhost:1",
+            "-t",
+            "1",
+            "--",
+            "echo",
+            "ok",
+        ],
+        capture_output=True,
     )
     assert proc.returncode == 0
